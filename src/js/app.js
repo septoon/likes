89d27a3c5e10c1780c75c0effa87ae05fe7d28f8
septoon <tigran.darchinyan@gmail.com>
2019-05-24 20:38:03 +0300

    image import fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
+import Like from '../img/like.png';
+
 const btn = document.getElementById('button');
 const wrapper = document.getElementById('wrapper');
 
 btn.addEventListener('click', () => {
   const img = document.createElement('img');
   const random = Math.floor(Math.random() * 4 + 1);
-  img.src = './img/like.png';
+  img.src = Like;
   img.classList = `like like-animate like-animate-${random}`;
   img.alt = 'like';
 
